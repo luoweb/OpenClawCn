a0ae77873c397d063adb92c6ec9678fe58918669
Robin <luoweb@users.noreply.github.com>
2026-03-03 22:58:37 +0800

    custom shell

--- custom.sh
+++ custom.sh
@@ -28,18 +28,18 @@ sed_inplace 's#https://qingchencloud.com/#https://roweb.cn/#g' package.json
 sed_inplace 's#武汉晴辰天下网络科技有限公司#roweb#g' .github/workflows/sync-and-release.yml
 sed_inplace 's#<contact@qingchencloud.com>#<contact@roweb.cn>#g' .github/workflows/sync-and-release.yml
 sed_inplace 's#https://qingchencloud.com/#https://roweb.cn/#g' .github/workflows/sync-and-release.yml
-sed_inplace 's#1186258278/OpenClawChineseTranslation#luoweb#openclawcn#g' .github/workflows/sync-and-release.yml
+sed_inplace 's#1186258278/OpenClawChineseTranslation#luoweb/openclawcn#g' .github/workflows/sync-and-release.yml
 sed_inplace 's#1186258278/openclaw-zh:latest#luoweb/openclaw-zh:latest#g' .github/workflows/sync-and-release.yml
 
 # 修改 Dockerfile
 sed_inplace 's#武汉晴辰天下网络科技有限公司#roweb#g' Dockerfile
 sed_inplace 's#<contact@qingchencloud.com>#<contact@roweb.cn>#g' Dockerfile
 sed_inplace 's#https://qingchencloud.com/#https://roweb.cn/#g' Dockerfile
-sed_inplace 's#1186258278/OpenClawChineseTranslation#luoweb#openclawcn#g' Dockerfile
+sed_inplace 's#1186258278/OpenClawChineseTranslation#luoweb/openclawcn#g' Dockerfile
 sed_inplace 's#1186258278/openclaw-zh:latest#luoweb/openclaw-zh:latest#g' Dockerfile
 
 # 修改 install.sh
 sed_inplace 's#武汉晴辰天下网络科技有限公司#roweb#g'  *.sh
 sed_inplace 's#https://qingchencloud.com/#https://roweb.cn/#g'  *.sh
-sed_inplace 's#1186258278/OpenClawChineseTranslation#luoweb#openclawcn#g'  *.sh
+sed_inplace 's#1186258278/OpenClawChineseTranslation#luoweb/openclawcn#g'  *.sh
 sed_inplace 's#https://roweb.cn/#https://luoweb.cn/#g'  *.sh
